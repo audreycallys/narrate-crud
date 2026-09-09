aeedeb5d2cccb82463546d7d9b55a9e6607ab0bb
audreycallys <nevaely0107@gmail.com>
2026-09-09 20:34:36 +0700

    feat: menambahkan validasi update

--- src/validations/posts/post.validation.ts
+++ src/validations/posts/post.validation.ts
@@ -24,3 +24,21 @@ export const createPostSchema = z.object({
 export const postIdSchema = z.object({
   id: z.coerce.number().int().positive("Post ID tidak valid"),
 });
+
+export const updatePostSchema = z.object({
+  categoryId: z.coerce.number().int().positive("Category wajib dipilih"),
+
+  authorName: z
+    .string()
+    .min(2, "Nama author minimal 2 karakter")
+    .max(100, "Nama author maksimal 100 karakter"),
+
+  title: z
+    .string()
+    .min(3, "Title minimal 3 karakter")
+    .max(255, "Title maksimal 255 karakter"),
+
+  content: z.string().min(10, "Content minimal 10 karakter"),
+
+  status: z.enum(["draft", "published", "archived"]),
+});
