263f935d7f644280d110e15f012fc16285436fdd
audreycallys <nevaely0107@gmail.com>
2026-09-10 00:15:16 +0700

    feat: menambahkan routes update profile

--- src/routes/profile/profile.route.ts
+++ src/routes/profile/profile.route.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import ProfileController from "../../controllers/profile/profile.controller";
+import { uploadSingleImage } from "../../middleware/upload.middleware";
 
 const router = Router();
 
 // Read Profile
 router.get("/", ProfileController.getProfile);
+
+// Update Profile
+router.put("/", uploadSingleImage, ProfileController.updateProfile);
 
 export default router;
