6bfed3ddb289cddded4afbe2eecfe1c10953adf6
audreycallys <nevaely0107@gmail.com>
2026-09-11 18:38:51 +0700

    feat: menambahkan controller import id url & update tag

--- src/controllers/tags/tags.controller.ts
+++ src/controllers/tags/tags.controller.ts
@@ -5,6 +5,8 @@ import { tagsTable, categoriesTable } from "../../config/schema";
 import {
   tagQuerySchema,
   createTagSchema,
+  tagIdSchema,
+  updateTagSchema,
 } from "../../validations/tags/tag.validation";
 
 export class TagsController {
@@ -77,6 +79,66 @@ export class TagsController {
       });
     } catch (error) {
       console.error("Create tag error:", error);
+
+      return res.status(500).json({
+        success: false,
+        message: "Terjadi kesalahan pada server",
+        error: error instanceof Error ? error.message : error,
+      });
+    }
+  };
+
+  // Update Tag
+  updateTag = async (req: Request, res: Response) => {
+    try {
+      const validatedParams = tagIdSchema.parse(req.params);
+      const { id } = validatedParams;
+
+      const validateData = updateTagSchema.parse(req.body);
+      const { categoryId, name } = validateData;
+
+      const [existingTag] = await db
+        .select()
+        .from(tagsTable)
+        .where(eq(tagsTable.id, id));
+
+      if (!existingTag) {
+        return res.status(404).json({
+          success: false,
+          message: "Tag Not Found",
+        });
+      }
+
+      const [category] = await db
+        .select()
+        .from(categoriesTable)
+        .where(eq(categoriesTable.id, categoryId));
+
+      if (!category) {
+        return res.status(404).json({
+          success: false,
+          message: "Category Not Found",
+        });
+      }
+
+      const [updatedTag] = await db
+        .update(tagsTable)
+        .set({
+          categoryId,
+          name,
+        })
+        .where(eq(tagsTable.id, id))
+        .returning();
+
+      return res.status(200).json({
+        success: true,
+        message: "Tag updated successfully",
+        data: {
+          tag: updatedTag,
+        },
+      });
+    } catch (error) {
+      console.error("Update tag error:", error);
 
       return res.status(500).json({
         success: false,
