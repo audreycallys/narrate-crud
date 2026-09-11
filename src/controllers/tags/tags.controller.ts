e66c364448537afb6705df5968c0c945335b23bd
audreycallys <nevaely0107@gmail.com>
2026-09-11 18:33:37 +0700

    feat: menambahkan controller tag

--- src/controllers/tags/tags.controller.ts
+++ src/controllers/tags/tags.controller.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
 import { asc, eq } from "drizzle-orm";
 import { db } from "../../config/db";
-import { tagsTable } from "../../config/schema";
-import { tagQuerySchema } from "../../validations/tags/tag.validation";
+import { tagsTable, categoriesTable } from "../../config/schema";
+import {
+  tagQuerySchema,
+  createTagSchema,
+} from "../../validations/tags/tag.validation";
 
 export class TagsController {
   // Membaca Semua Tags
@@ -29,6 +32,51 @@ export class TagsController {
       });
     } catch (error) {
       console.error("Get tags error:", error);
+
+      return res.status(500).json({
+        success: false,
+        message: "Terjadi kesalahan pada server",
+        error: error instanceof Error ? error.message : error,
+      });
+    }
+  };
+
+  // Membuat Tag
+  createTag = async (req: Request, res: Response) => {
+    try {
+      const validateData = createTagSchema.parse(req.body);
+
+      const { categoryId, name } = validateData;
+
+      const [category] = await db
+        .select()
+        .from(categoriesTable)
+        .where(eq(categoriesTable.id, categoryId));
+
+      if (!category) {
+        return res.status(404).json({
+          success: false,
+          message: "Category Not Found",
+        });
+      }
+
+      const [newTag] = await db
+        .insert(tagsTable)
+        .values({
+          categoryId,
+          name,
+        })
+        .returning();
+
+      return res.status(201).json({
+        success: true,
+        message: "Tag created successfully",
+        data: {
+          tag: newTag,
+        },
+      });
+    } catch (error) {
+      console.error("Create tag error:", error);
 
       return res.status(500).json({
         success: false,
