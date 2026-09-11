3a7598358b861b8bffc994d83f7fe1b008f35a36
audreycallys <nevaely0107@gmail.com>
2026-09-11 18:37:48 +0700

    feat: menambahkan validasi id url & update tag

--- src/validations/tags/tag.validation.ts
+++ src/validations/tags/tag.validation.ts
@@ -16,3 +16,16 @@ export const createTagSchema = z.object({
     .min(2, "Nama tag minimal 2 karakter")
     .max(50, "Nama tag maksimal 50 karakter"),
 });
+
+export const tagIdSchema = z.object({
+  id: z.coerce.number().int().positive("Tag ID tidak valid"),
+});
+
+export const updateTagSchema = z.object({
+  categoryId: z.coerce.number().int().positive("Category ID tidak valid"),
+
+  name: z
+    .string()
+    .min(2, "Nama tag minimal 2 karakter")
+    .max(50, "Nama tag maksimal 50 karakter"),
+});
