bf659d53715c240d7946f331ceb639bc7a0f7981
audreycallys <nevaely0107@gmail.com>
2026-09-11 17:53:40 +0700

    refactor: hapus authorname dari validasi post

--- src/validations/posts/post.validation.ts
+++ src/validations/posts/post.validation.ts
@@ -28,11 +28,6 @@ export const postIdSchema = z.object({
 export const updatePostSchema = z.object({
   categoryId: z.coerce.number().int().positive("Category wajib dipilih"),
 
-  authorName: z
-    .string()
-    .min(2, "Nama author minimal 2 karakter")
-    .max(100, "Nama author maksimal 100 karakter"),
-
   title: z
     .string()
     .min(3, "Title minimal 3 karakter")
